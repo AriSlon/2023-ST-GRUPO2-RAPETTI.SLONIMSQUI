5c7c8c657650fa3d248d734461ea5b103b5bd35f
Ari Slonimsqui <arislonimsqui@gmail.com>
2023-04-18 14:39:30 -0300

    hols

--- Trabajo_Practico_Maquina_de_Estados__Reloj_Ej_1/Trabajo_Practico_Maquina_de_Estados__Reloj_Ej_1.ino
+++ Trabajo_Practico_Maquina_de_Estados__Reloj_Ej_1/Trabajo_Practico_Maquina_de_Estados__Reloj_Ej_1.ino
@@ -2,7 +2,9 @@
   Seminario de Informatica y Telecomunicaciones
   Clase 11/4 - TP Maquina de Estados Reloj - Ejercicio 1
 
-  Hecho por Santiago Rapetti y Ariel Slonimsqui
+  Hecho por Santiago Rapetti y Ariel Slonimsqui.
+
+  Comentarios: La hora esta inicializada en 0 horas y 0 minutos.
 */
 
 #include <Wire.h>
@@ -12,7 +14,6 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <ESP32Time.h>
 
 #define SCREEN_WIDTH 128 // Anchura OLED display en pixeles
 #define SCREEN_HEIGHT 64 // Altura OLED display en pixeles
@@ -20,15 +21,14 @@
 #define DHTPIN 23     // Pin digital conectado al sensor DHT
 #define DHTTYPE    DHT11     // DHT 11
 
-#define RELAY 18
 #define SW_1 34
 #define SW_2 35
 
-#define PANTALLA_1 0 //muestro temperatura y hora 
+#define PANTALLA_1 0 //muestro temperatura y umbral 
 #define ESPERA_1 1 //apretar y soltar botones
-#define PANTALLA_2 2 // regular temperatura
-#define SUMA_HORAS 3 //
-#define SUMA_MINUTOS 4 //
+#define PANTALLA_2 2 // muestro umbral
+#define SUMA_MINUTOS 3 //sumo minutos
+#define SUMA_HORA 4 //sumo horas
 #define ESPERA_2  5
 
 int estadoMaquina;
@@ -36,12 +36,11 @@ int estadoMaquina;
 int estadoSwitch1;
 int estadoSwitch2;
 
-int segundos;
-int minutos;
 int hora;
-int dia;
-int mes;
-int year;
+int minutos;
+
+unsigned long milisActuales;
+unsigned long milisPrevios;
 
 float temp;
 
@@ -51,26 +50,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 sensor_t sensor;
 
-ESP32Time rtc;
-
 void setup() {
-
-  Serial.begin(115200);
-
-  segundos = 0;
-  minutos = 58;
-  hora = 14;
-  dia = 11;
-  mes = 4;
-  year = 2023;
 
   pinMode(SW_1, INPUT_PULLUP);
   pinMode(SW_2, INPUT_PULLUP);
 
+
+  Serial.begin(115200);
+
   dht.begin();
-
-
-  rtc.setTime(segundos, minutos, hora, dia, mes, year);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -83,28 +71,36 @@ void loop() {
 
   estadoSwitch1 = digitalRead(SW_1);
   estadoSwitch2 = digitalRead(SW_2);
-  Serial.println(minutos);
-  minutos = rtc.getMinute();
-  hora = rtc.getHour();
 
+  Serial.println(estadoMaquina);
 
-  //Serial.println(estadoMaquina);
+  milisActuales = millis(); // Guarda el tiempo en milisegundos desde que se inicio el programa
 
-  maquinaDeEstados();
+  if ((milisActuales - milisPrevios) >= 60000) {
+    minutos = minutos + 1;
+    milisPrevios = milisActuales;
 
-  if (hora > 23) {
-    hora = 0;
+  }
+
+  if ((milisActuales - milisPrevios) >= 3600000) {
+    hora = hora + 1;
+    milisPrevios = milisActuales;
   }
 
   if (minutos > 59) {
     minutos = 0;
+    hora = hora + 1;
   }
 
+  if (hora > 23) {
+    hora = 0;
 
+  }
+  maquinaDeEstados();
 
 }
 
-void maquinaDeEstados () {
+void maquinaDeEstados() {
 
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -115,8 +111,6 @@ void maquinaDeEstados () {
     case PANTALLA_1:
 
       pantalla1();
-
-      rtc.setTime(segundos, minutos, hora, dia, mes, 2023);
 
       if (estadoSwitch1 == LOW &&  estadoSwitch2 == LOW) {
         estadoMaquina = 1;
@@ -138,10 +132,6 @@ void maquinaDeEstados () {
 
       pantalla2();
 
-      if (estadoSwitch1 == LOW &&  estadoSwitch2 == LOW) {
-        estadoMaquina = 5;
-      }
-
       if (estadoSwitch1 == LOW) {
 
         estadoMaquina = 3;
@@ -152,20 +142,8 @@ void maquinaDeEstados () {
         estadoMaquina = 4;
       }
 
-
-      break;
-
-    case SUMA_HORAS:
-
-      pantalla2();
-
       if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
         estadoMaquina = 5;
-      }
-
-      if (estadoSwitch1 == HIGH) {
-        hora = hora + 1;
-        estadoMaquina = 2;
       }
 
       break;
@@ -174,13 +152,32 @@ void maquinaDeEstados () {
 
       pantalla2();
 
+
       if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
         estadoMaquina = 5;
       }
 
-      if (estadoSwitch2 == HIGH) {
+      if (estadoSwitch1 == HIGH) {
         minutos = minutos + 1;
+
         estadoMaquina = 2;
+      }
+
+
+      break;
+
+    case SUMA_HORA:
+
+      pantalla2();
+
+      if (estadoSwitch2 == HIGH) {
+        hora = hora + 1;
+
+        estadoMaquina = 2;
+      }
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = 5;
       }
 
       break;
@@ -188,8 +185,6 @@ void maquinaDeEstados () {
     case ESPERA_2:
 
       pantalla2();
-      rtc.setTime(segundos, minutos, hora, dia, mes, 2023);
-
 
       if ( estadoSwitch1 == HIGH && estadoSwitch2 == HIGH) {
         estadoMaquina = 0;
@@ -199,7 +194,6 @@ void maquinaDeEstados () {
 
   }
 }
-
 
 void pantalla1() {
 
@@ -211,15 +205,13 @@ void pantalla1() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("Temperatura: ");
+  display.println("Temperatura Actual: ");
   display.println(event.temperature);
   display.println("Hora: ");
   display.print(hora);
   display.print(":");
   display.print(minutos);
-
   display.display();
-
 }
 
 void pantalla2() {
@@ -228,10 +220,9 @@ void pantalla2() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
+  display.println("Hora: ");
   display.print(hora);
   display.print(":");
   display.print(minutos);
-
   display.display();
-
 }
