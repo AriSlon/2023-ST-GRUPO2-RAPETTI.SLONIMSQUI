732104795d2f74dade24c20139e1c90f79140a4f
Ari Slonimsqui <arislonimsqui@gmail.com>
2023-05-16 15:05:03 -0300

    renovado

--- TP3/TP3.ino
+++ TP3/TP3.ino
@@ -1,3 +1,10 @@
+/*
+  Seminario de Informatica y Telecomunicaciones
+  Clase 2/5 - TP 3 Telegram
+  Hecho por Santiago Rapetti y Ariel Slonimsqui.
+  Comentario: El codigo puede andar lento por momentos y, por ejemplo, tardar en cambiar de estados.
+*/
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -8,42 +15,58 @@
 
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
 #define SCREEN_WIDTH 128 // Anchura OLED display en pixeles
 #define SCREEN_HEIGHT 64 // Altura OLED display en pixeles
 
 #define DHTPIN 23     // Pin digital conectado al sensor DHT
-#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT11     // DHT 1
 
 #define SW_1 34
 #define SW_2 35
 
-#define BOTtoken "6128870296:AAGyVnwhAqvWyJD2jCMDukpjcu15meeOXkI"  // your Bot Token (Get from Botfather)
-#define CHAT_ID "842740065"
+#define PANTALLA_1 0
+#define ESPERA_1 1
+#define ESPERA_2 2
+#define ESPERA_3 3
+#define ESPERA_4 4
+#define ESPERA_5 5
+#define PANTALLA_2 6
+#define SUMA_UMBRAL 7
+#define RESTA_UMBRAL 8
+#define ESPERA_6 9
 
-#define PANTALLA_1 0 //muestro temperatura y umbral 
-#define ESPERA_1 1 //apretar y soltar botones
-#define PANTALLA_2 2 // muestro umbral
-#define SUMA 3 //sumo minutos
-#define RESTA 4 //sumo horas
-#define ESPERA_2  5
+#define BOTtoken "6128870296:AAGyVnwhAqvWyJD2jCMDukpjcu15meeOXkI"  // cambiar por token 
+
+#define CHAT_ID "5939693005"
+
+const char* ssid = "Ari";
+const char* password = "004367225aa";
 
 int estadoMaquina;
 
 int estadoSwitch1;
 int estadoSwitch2;
 
-int hora;
-int minutos;
+int valorUmbral = 22;
 
-int valorUmbral;
+String mensaje = "La temperatura actual es: ";
+
+
+int botRequestDelay = 1000; /// intervalo
+unsigned long lastTimeBotRan; /// ultimo tiempo
+
+float temp;
 
 unsigned long milisActuales;
 unsigned long milisPrevios;
 
-float temp;
+unsigned long milisActuales2;
+unsigned long milisPrevios2;
+
+boolean flagTemperatura;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -51,23 +74,243 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 sensor_t sensor;
 
-// credenciales de wifi
-const char* ssid = "ORT-IoT";
-const char* password = "OrtIOTnew22$2";
-
-
-
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
+void setup() {
 
-int botRequestDelay = 1000; /// intervalo
-unsigned long lastTimeBotRan; /// ultimo tiempo
+  pinMode(SW_1, INPUT_PULLUP);
+  pinMode(SW_2, INPUT_PULLUP);
 
-const int ledPin = 2; /// pin led
-bool ledState = LOW;
 
-// funcion que se comunica con telegram
+  Serial.begin(115200);
+
+  dht.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println(WiFi.localIP());
+  bot.sendMessage(CHAT_ID, "Conexion establecida entre el Bot y el microcontrolador", "");
+}
+
+
+void loop() {
+
+  Serial.println(estadoMaquina);
+
+  estadoSwitch1 = digitalRead(SW_1);
+  estadoSwitch2 = digitalRead(SW_2);
+
+  milisActuales = millis();
+  milisActuales2 = millis();
+
+
+  if((milisActuales2 - milisPrevios2) >= 1000){
+    
+    lecturaTempreatura();
+    milisPrevios2 = milisActuales2;
+    
+  }
+
+  if (valorUmbral < 0) {
+    valorUmbral = 0;
+  }
+
+  if (flagTemperatura == 0) {
+
+    if (temp > valorUmbral) {
+
+      flagTemperatura = 1;
+
+      bot.sendMessage(CHAT_ID, "La temperatura supero el valor umbral!!!", "");
+    }
+
+  }
+
+  if (flagTemperatura == 1) {
+
+
+    if (temp < valorUmbral) {
+
+      flagTemperatura = 0;
+
+      bot.sendMessage(CHAT_ID, "La temperatura es menor al valor umbral", "");
+
+
+    }
+
+  }
+
+  lecturaTiempoBot();
+
+  maquinaDeEstados();
+
+}
+
+void maquinaDeEstados() {
+
+  switch (estadoMaquina) {
+
+
+    case PANTALLA_1:
+
+      pantalla1();
+
+      if (estadoSwitch1 == LOW) {
+        estadoMaquina = ESPERA_1;
+      }
+
+      break;
+
+    case ESPERA_1:
+
+      pantalla1();
+
+      if (estadoSwitch1 == HIGH) {
+        estadoMaquina = ESPERA_2;
+      }
+
+      break;
+
+    case ESPERA_2:
+
+      pantalla1();
+
+      if (estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_3;
+      }
+
+      break;
+
+    case ESPERA_3:
+
+      pantalla1();
+
+      if (estadoSwitch2 == HIGH) {
+        milisPrevios = milisActuales;
+        estadoMaquina = ESPERA_4;
+      }
+
+      break;
+
+    case ESPERA_4:
+
+      pantalla1();
+
+      if (estadoSwitch1 == LOW) {
+        estadoMaquina = ESPERA_5;
+      }
+
+      if ((milisActuales - milisPrevios) >= 5000) {
+        milisPrevios = milisActuales;
+        estadoMaquina = PANTALLA_1;
+      }
+
+      break;
+
+    case ESPERA_5:
+
+      pantalla1();
+
+      if (estadoSwitch1 == HIGH) {
+        estadoMaquina = PANTALLA_2;
+      }
+
+      break;
+
+    case PANTALLA_2:
+
+      pantalla2();
+
+      if (estadoSwitch1 == LOW) {
+        estadoMaquina = SUMA_UMBRAL;
+      }
+
+      if (estadoSwitch2 == LOW) {
+        estadoMaquina = RESTA_UMBRAL;
+      }
+
+      if (estadoSwitch1 == LOW &&  estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_6;
+      }
+
+      break;
+
+    case SUMA_UMBRAL:
+
+      pantalla2();
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_6;
+      }
+
+      if (estadoSwitch1 == HIGH) {
+        valorUmbral = valorUmbral + 1;
+
+        estadoMaquina = PANTALLA_2;
+      }
+
+      break;
+
+    case RESTA_UMBRAL:
+
+      pantalla2();
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_6;
+      }
+
+      if (estadoSwitch2 == HIGH) {
+        valorUmbral = valorUmbral - 1;
+
+        estadoMaquina = PANTALLA_2;
+      }
+
+      break;
+
+    case ESPERA_6:
+
+      pantalla2();
+
+      if (estadoSwitch1 == HIGH && estadoSwitch2 == HIGH) {
+        estadoMaquina = PANTALLA_1;
+      }
+
+      break;
+
+  }
+
+}
+
+void lecturaTiempoBot () {
+
+  if (millis() > lastTimeBotRan + botRequestDelay) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages) {
+      Serial.println("Veo los msj nuevos");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+
+  }
+
+
+}
+
 void handleNewMessages(int numNewMessages) {
   Serial.println("Mensaje nuevo");
   Serial.println(String(numNewMessages));
@@ -88,195 +331,24 @@ void handleNewMessages(int numNewMessages) {
     String from_name = bot.messages[i].from_name;
 
     /// si rebice /led on enciende el led
-    if (text == "/led_on") {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
+    if (text == "/temperatura_actual") {
+      bot.sendMessage(chat_id, (String)temp, "");
     }
 
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
-
-    if (text == "/state") {
-      if (digitalRead(ledPin)) {
-        bot.sendMessage(CHAT_ID, "LED is ON", "");
-      }
-      else {
-        bot.sendMessage(CHAT_ID, "LED is OFF", "");
-      }
-    }
   }
 
-
-}
-
-void setup() {
-
-  pinMode(SW_1, INPUT_PULLUP);
-  pinMode(SW_2, INPUT_PULLUP);
-
-
-  Serial.begin(115200);
-
-  dht.begin();
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
-
-  // Connect to Wi-Fi
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
-
-  ///bloquea el programa si no se puede conectar a internet
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-  bot.sendMessage(CHAT_ID, "Bot Hola mundo", "");
-}
-
-void loop() {
-
-  estadoSwitch1 = digitalRead(SW_1);
-  estadoSwitch2 = digitalRead(SW_2);
-
-  Serial.println(estadoMaquina);
-
-  milisActuales = millis(); // Guarda el tiempo en milisegundos desde que se inicio el programa
-
-  if ((milisActuales - milisPrevios) >= 100) {
-
-    maquinaDeEstados();
-    milisPrevios = milisActuales;
-
-  }
-
-  maquinaDeEstados();
-
-}
-
-void maquinaDeEstados() {
-
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  temp = event.temperature;
-
-  switch (estadoMaquina) {
-
-    case PANTALLA_1:
-
-      pantalla1();
-
-      if (temp > valorUmbral) {
-        bot.sendMessage(CHAT_ID, "Umbral de temperatura superado", "");
-
-      } else {
-
-      }
-
-
-      if (estadoSwitch1 == LOW &&  estadoSwitch2 == LOW) {
-        estadoMaquina = 1;
-      }
-
-      break;
-
-    case ESPERA_1:
-
-      pantalla1();
-
-      if (estadoSwitch1 == HIGH &&  estadoSwitch2 == HIGH) {
-        estadoMaquina = 2;
-      }
-
-      break;
-
-    case PANTALLA_2:
-
-      pantalla2();
-
-      if (estadoSwitch1 == LOW) {
-
-        estadoMaquina = 3;
-      }
-
-      if (estadoSwitch2 == LOW) {
-
-        estadoMaquina = 4;
-      }
-
-      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
-        estadoMaquina = 5;
-      }
-
-      break;
-
-    case SUMA:
-
-      pantalla2();
-
-
-      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
-        estadoMaquina = 5;
-      }
-
-      if (estadoSwitch1 == HIGH) {
-        valorUmbral = valorUmbral + 1;
-
-        estadoMaquina = 2;
-      }
-
-
-      break;
-
-    case RESTA:
-
-      pantalla2();
-
-      if (estadoSwitch2 == HIGH) {
-        valorUmbral = valorUmbral - 1;
-
-        estadoMaquina = 2;
-      }
-
-      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
-        estadoMaquina = 5;
-      }
-
-      break;
-
-    case ESPERA_2:
-
-      pantalla2();
-
-      if ( estadoSwitch1 == HIGH && estadoSwitch2 == HIGH) {
-        estadoMaquina = 0;
-      }
-
-      break;
-
-  }
 }
 
 void pantalla1() {
-
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  temp = event.temperature;
 
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.println("Temperatura Actual: ");
-  display.println(event.temperature);
+  display.println(temp);
   display.println("Valor Umbral: ");
-  display.println(valorUmbral);
+  display.print(valorUmbral);
   display.display();
 }
 
@@ -286,8 +358,16 @@ void pantalla2() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("Valor Umbral: ");
+  display.print("Valor Umbral: ");
   display.println(valorUmbral);
   display.display();
+
+}
+
+void lecturaTempreatura() {
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  temp = event.temperature;
 
 }
---
+++ TP_Opcional_Hakaton/TP_Opcional_Hakaton.ino
@@ -0,0 +1,357 @@
+/*
+  Seminario de Informatica y Telecomunicaciones
+  Clase 25/4 - TP Opcional Riego
+  Hecho por Santiago Rapetti y Ariel Slonimsqui.
+*/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define SCREEN_WIDTH 128 // Anchura OLED display en pixeles
+#define SCREEN_HEIGHT 64 // Altura OLED display en pixeles
+
+#define DHTPIN 23     // Pin digital conectado al sensor DHT
+#define DHTTYPE    DHT11     // DHT 11
+
+#define SW_1 34
+#define SW_2 35
+#define LED 25
+
+#define PANTALLA_1 0 //muestro temperatura y umbral 
+#define ESPERA_1 1 //apretar y soltar botones
+#define PANTALLA_2 2 // muestro umbral
+#define SUMA_TIEMPO_CICLO 3 //sumo minutos
+#define RESTA_TIEMPO_CICLO 4 //sumo horas
+#define ESPERA_2  5
+#define PANTALLA_3 6
+#define SUMA_TIEMPO_RIEGO 7 //sumo minutos
+#define RESTA_TIEMPO_RIEGO 8 //sumo horas
+#define ESPERA_3  9
+
+
+int estadoMaquina;
+int estadoMaquinaRiego;
+
+int estadoSwitch1;
+int estadoSwitch2;
+
+int hora;
+int minutos;
+
+int tiempoRiego = 30;
+int intervaloRiego = 60;
+
+unsigned long milisActuales;
+unsigned long milisPrevios;
+
+int timer;
+
+float temp;
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+sensor_t sensor;
+
+void setup() {
+
+  pinMode(SW_1, INPUT_PULLUP);
+  pinMode(SW_2, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
+
+
+  Serial.begin(115200);
+
+  dht.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+}
+
+void loop() {
+
+  estadoSwitch1 = digitalRead(SW_1);
+  estadoSwitch2 = digitalRead(SW_2);
+
+  Serial.println(estadoMaquina);
+
+  milisActuales = millis(); // Guarda el tiempo en milisegundos desde que se inicio el programa
+
+  if ((milisActuales - milisPrevios) >= 1000) {
+
+    timer = timer + 1;
+
+    milisPrevios = milisActuales;
+
+  }
+  maquinaDeEstadosRiego ();
+
+  maquinaDeEstados();
+
+  if (intervaloRiego < 60) {
+    intervaloRiego = 60;
+  }
+
+  if (tiempoRiego < 30) {
+    tiempoRiego = 30;
+  }
+
+
+}
+
+void maquinaDeEstados() {
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  temp = event.temperature;
+
+  switch (estadoMaquina) {
+
+    case PANTALLA_1:
+
+      pantalla1();
+
+
+
+      if (estadoSwitch1 == LOW &&  estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_1;
+      }
+
+      break;
+
+    case ESPERA_1:
+
+      pantalla1();
+
+      if (estadoSwitch1 == HIGH &&  estadoSwitch2 == HIGH) {
+        estadoMaquina = PANTALLA_2;
+      }
+
+      break;
+
+    case PANTALLA_2:
+
+      pantalla2();
+
+      if (estadoSwitch2 == LOW) {
+
+        estadoMaquina = SUMA_TIEMPO_CICLO;
+      }
+
+      if (estadoSwitch1 == LOW) {
+
+        estadoMaquina = RESTA_TIEMPO_CICLO;
+      }
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_2;
+      }
+
+      break;
+
+    case SUMA_TIEMPO_CICLO:
+
+      pantalla2();
+
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_2;
+      }
+
+      if (estadoSwitch2 == HIGH) {
+        intervaloRiego = intervaloRiego + 60;
+
+        estadoMaquina = PANTALLA_2;
+      }
+
+
+      break;
+
+    case RESTA_TIEMPO_CICLO:
+
+      pantalla2();
+
+      if (estadoSwitch1 == HIGH) {
+        intervaloRiego = intervaloRiego - 60;
+
+        estadoMaquina = PANTALLA_2;
+      }
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_2;
+      }
+
+      break;
+
+    case ESPERA_2:
+
+      pantalla2();
+
+      if ( estadoSwitch1 == HIGH && estadoSwitch2 == HIGH) {
+        estadoMaquina = PANTALLA_3;
+      }
+
+      break;
+
+    case PANTALLA_3:
+
+      pantalla3();
+
+      if (estadoSwitch2 == LOW) {
+
+        estadoMaquina = SUMA_TIEMPO_RIEGO;
+      }
+
+      if (estadoSwitch1 == LOW) {
+
+        estadoMaquina = RESTA_TIEMPO_RIEGO;
+      }
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_3;
+      }
+
+      break;
+
+    case SUMA_TIEMPO_RIEGO:
+
+      pantalla3();
+
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_3;
+      }
+
+      if (estadoSwitch2 == HIGH) {
+        tiempoRiego = tiempoRiego + 30;
+
+        estadoMaquina = PANTALLA_3;
+      }
+
+
+      break;
+
+    case RESTA_TIEMPO_RIEGO:
+
+      pantalla3();
+
+      if (estadoSwitch1 == HIGH) {
+        tiempoRiego = tiempoRiego - 30;
+
+        estadoMaquina = PANTALLA_3;
+      }
+
+      if (estadoSwitch1 == LOW && estadoSwitch2 == LOW) {
+        estadoMaquina = ESPERA_3;
+      }
+
+      break;
+
+    case ESPERA_3:
+
+      pantalla3();
+
+      if ( estadoSwitch1 == HIGH && estadoSwitch2 == HIGH) {
+        estadoMaquina = PANTALLA_1;
+      }
+
+      break;
+
+  }
+}
+
+void maquinaDeEstadosRiego () {
+
+  switch (estadoMaquinaRiego) {
+
+    case 0:
+
+      if (timer >= intervaloRiego) {
+        digitalWrite(LED, HIGH);
+        timer = 0;
+
+        estadoMaquinaRiego = 1;
+      }
+
+      break;
+
+    case 1:
+
+      if (timer >= tiempoRiego) {
+        digitalWrite(LED, LOW);
+        timer = 0;
+
+        estadoMaquinaRiego = 0;
+
+      }
+
+      break;
+
+  }
+
+}
+
+void pantalla1() {
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  temp = event.temperature;
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Temperatura Actual: ");
+  display.println(event.temperature);
+  display.println();
+  display.print("Riego: ");
+
+  if (digitalRead(LED) == HIGH) {
+    display.println("Activado");
+  }
+  if (digitalRead(LED) == LOW) {
+    display.println("Desactivado");
+  }
+
+  display.println();
+  display.print("Ciclo: ");
+  display.print(intervaloRiego / 60);
+  display.println(" minuto/s");
+  display.display();
+
+}
+
+void pantalla2() {
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("Ciclo: ");
+  display.print(intervaloRiego / 60);
+  display.println(" minuto/s");
+  display.display();
+}
+
+void pantalla3() {
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("Tiempo Riego: ");
+  display.print(tiempoRiego);
+  display.println(" segs");
+
+  display.display();
+}
